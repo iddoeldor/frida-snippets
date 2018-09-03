ad9847109963df49bdc497484f74a02d2ad3a53e
iddoeldor <iddo.eldor.work@gmail.com>
2018-09-04 02:57:54 +0300

    bash colors

--- scripts/trace_class.js
+++ scripts/trace_class.js
@@ -1,4 +1,18 @@
 // $ frida -Uf com.whatsapp --no-pause -l wa.js
+/*
+#!/bin/bash
+for fgbg in 38 48 ; do # Foreground / Background
+    for color in {0..255} ; do # Colors
+        # Display the color
+        printf "\e[${fgbg};5;%sm  %3s  \e[0m" $color $color
+        # Display 6 colors per lines
+        if [ $((($color + 1) % 6)) == 4 ] ; then
+            echo # New line
+        fi
+    done
+    echo # New line
+done
+*/
 var Color = {
     RESET: "\x1b[39;49;00m", Black: "0;01", Blue: "4;01", Cyan: "6;01", Gray: "7;11", Green: "2;01", Purple: "5;01", Red: "1;01", Yellow: "3;01",
     Light: {
