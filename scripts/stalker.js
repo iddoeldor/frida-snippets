25bdd40c6908d07a0024b4523613fb16c2a7c698
iddoeldor <iddo.eldor.work@gmail.com>
2019-05-16 19:52:37 +0300

    add log to stalker

--- scripts/stalker.js
+++ scripts/stalker.js
@@ -1,6 +1,12 @@
 var sendText = ObjC.classes.PhoneService['- sendText:to:withSeq:'];
 Interceptor.attach(ObjC.classes.MyClass['- myMethod:param1'].implementation, {
   onEnter: function (args) {
+    console.warn(JSON.stringify({
+      fname: args[1].readCString(),
+      text: new ObjC.Object(args[2]).toString(),
+      backtrace: Thread.backtrace(this.context, Backtracer.ACCURATE).map(DebugSymbol.fromAddress).map(m => m.moduleName+'!'+m.name),
+      ctx: this.context
+    }, null, 2));    
     var tid = Process.getCurrentThreadId();
     this.tid = tid;
     Stalker.follow(tid, {
